f1cfead5ebca8ab2570d3630382f324fc7c9d84b
YourAlly <Royettemiranda@YAHOO.COM>
2020-06-22 07:00:58 +0800

    Commit 22/06

--- calculator.js
+++ calculator.js
@@ -13,30 +13,47 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         selector.onchange = table_change;
     })
-    
+
     // Creates Table Once
     table_change();
 })
 
 function table_change() {
     var column_values_length = COLUMN_VALUES.length;
-    
+
     const rows = parseInt(document.querySelector('#rows').value);
     const cols = parseInt(document.querySelector('#cols').value);
-    
+
     // Head selectors
     var head = document.querySelector('#table-header');
     head.innerHTML = '';
-    for (var i = 0; i < cols; i++){
+    for (var i = 0; i < cols; i++) {
         var select = document.createElement("select");
-        select.dataset.column = 1;
-        for (var j = 0; j < column_values_length; j++){
+        select.className = 'select-area';
+        for (var j = 0; j < column_values_length; j++) {
             var item = document.createElement('option');
             item.value = COLUMN_VALUES[j];
             item.innerHTML = COLUMN_VALUES[j];
 
             select.append(item);
         }
-        head.append(select)
+        head.append(select);
     }
+
+    // Table input fields
+    var body = document.querySelector('#table-body');
+    body.innerHTML = '';
+    for (var i = 0; i < rows; i++) {
+        row = document.createElement('div');
+        row.className += `row-${i + 1}`;
+        for (var j = 0; j < cols; j++) {
+            var field = document.createElement('input');
+            field.type = 'text';
+            field.className += `input-area col-${j + 1}`;
+            row.append(field);
+        }
+        body.append(row);
+        body.append(document.createElement('br'))
+    }
+
 }
