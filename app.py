615e1d76d0e16cf63a18d353ffc91b0811db86ab
YourAlly <Royettemiranda@YAHOO.COM>
2020-06-22 21:29:03 +0800

    Help
    I still have no clear idea on what to do next, but I'm getting closer

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 
@@ -8,4 +8,21 @@ def index():
 
 @app.route("/submit", methods=["POST"])
 def submit():
+    row_num = request.form.get('rows')
+    col_num = request.form.get('cols')
+    columns_values = {}
+    head_names = set()
+    for i in range(int(col_num)):
+        if request.form.get(f'col-header-{i + 1}') in head_names:
+            return redirect(url_for('error'))
+        head_names.add(request.form.get(f'col-header-{i + 1}'))
+        if '' in request.form.getlist(f'col-{i + 1}'):
+            return redirect(url_for('error'))
+        columns_values[request.form.get(f'col-header-{i + 1}')] = request.form.getlist(f'col-{i + 1}')
+    print(columns_values)
     return redirect(url_for('index'))
+
+
+@app.route("/error")
+def error():
+    return "ERROR"
--- static/calculator.js
+++ static/calculator.js
@@ -6,13 +6,16 @@ document.addEventListener('DOMContentLoaded', () => {
         max = parseInt(selector.dataset.max);
         min = parseInt(selector.dataset.min)
 
+        // Fills it with the options available
         for (var i = min; i <= max; i++) {
             var item = document.createElement('option');
             item.value = i;
             item.innerHTML = i;
             selector.append(item);
         }
-        selector.onchange =  table_change;
+
+        // It looks cooler when you do it like this
+        selector.onchange = () => {table_change();};
     })
 
     // Creates Table Once
@@ -27,14 +30,18 @@ function table_change() {
     var head = document.querySelector('#table-header');
     head.innerHTML = '';
     for (var i = 0; i < cols; i++) {
-        
         var select = document.createElement("select");
-        select.className = `select-area col-header-${i + 1}`;
+        select.className = `select-area`;
+        select.name = `col-header-${i + 1}`;
         for (var j = 0; j < COLUMN_VALUES.length; j++) {
             var item = document.createElement('option');
             item.value = COLUMN_VALUES[j];
             item.innerHTML = COLUMN_VALUES[j];
 
+            // Just to make it look nicer
+            if (i === j) {
+                item.selected = "selected";
+            }
             select.append(item);
         }
         head.append(select);
@@ -45,12 +52,13 @@ function table_change() {
     body.innerHTML = '';
     for (var i = 0; i < rows; i++) {
         row = document.createElement('div');
-        row.className += `row-${i + 1}`;
+        row.className = `row-${i + 1}`;
 
         for (var j = 0; j < cols; j++) {
             var field = document.createElement('input');
             field.type = 'text';
-            field.className += `input-area col-${j + 1}`;
+            field.className = 'input-area';
+            field.name = `col-${j + 1}`
             row.append(field);
         }
         body.append(row);
